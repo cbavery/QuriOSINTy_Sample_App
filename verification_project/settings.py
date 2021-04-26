3761bc0937108d413f3f5e79e55db2d6379b04cd
Colton Bavery <coltonbavery2@gmail.com>
2021-04-26 11:37:03 -0400

    changed settings due to error, attempting fix

--- verification_project/settings.py
+++ verification_project/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 # Activate Django-Heroku.
